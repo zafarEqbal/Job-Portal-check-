10ef64528f69fdc854e8618b524356672002b218
Zafar <zafareqbalahmad08029@gmail.com>
2025-07-14 14:54:53 +0530

    Fixed all API endpoints for deployment

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({extended:true})) ;
 app.use(cookieParser()) ;
 
 const corsOptions = {
-    origin: 'http://localhost:5173' ,
+    origin: 'https://job-portal-check-3.onrender.com' ,
     credentials : true 
 }
 app.use(cors(corsOptions)) ;
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,6 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user" ;
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job" ;
-export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application" ;
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company" ;
+export const BASE_API = "https://job-portal-check-3.onrender.com/api/v1";
+
+export const USER_API_END_POINT = `${BASE_API}/user`;
+export const JOB_API_END_POINT = `${BASE_API}/job`;
+export const APPLICATION_API_END_POINT = `${BASE_API}/application`;
+export const COMPANY_API_END_POINT = `${BASE_API}/company`;
