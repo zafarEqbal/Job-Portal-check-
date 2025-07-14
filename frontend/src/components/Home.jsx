341a78734f8fecc44778cd132e83f328d08615e9
Zafar <zafareqbalahmad08029@gmail.com>
2025-07-14 13:54:24 +0530

    Fixed HeroSection import casing for deployment

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
-import HeroSection from './heroSection'
+import HeroSection from "./HeroSection";
 import CategoryCarousel from './CategoryCarousel'
 import LatestJobs from './LatestJobs'
 import Footer from './Footer'
