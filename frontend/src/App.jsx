c86ff599da67413b4944d1a23c453fcdcece78d6
Zafar <zafareqbalahmad08029@gmail.com>
2025-07-14 13:45:55 +0530

    Fix import casing for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Jobs from './components/Jobs';
 import Browse from './components/Browse';
 import Profile from './components/Profile';
 import JobDescription from './components/JobDescription';
-import Companies from './components/admin/companies';
+import Companies from './components/admin/Companies';
 import CompanyCreate from './components/admin/CompanyCreate';
 import CompanySetup from './components/admin/CompanySetup';
 import AdminJobs from './components/admin/AdminJobs';
